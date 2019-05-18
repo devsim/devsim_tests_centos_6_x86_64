1b191866e9a3922d5ac331d681f55c0ab0eb0ec2
Juan E. Sanchez <juan@tcad.com>
2019-05-18 09:29:29 -0500

    add package back

--- preinstall.sh
+++ preinstall.sh
@@ -7,5 +7,7 @@ if [ ! -d ${HOME}/anaconda ]; then
 ( cd ${HOME} && curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh  && bash Miniconda3-latest-Linux-x86_64.sh -b -p ${HOME}/anaconda )
 fi
 
+${HOME}/anaconda/bin/conda install -y --name base cmake
 ${HOME}/anaconda/bin/conda create  -y --name python37_devsim python=3.7
 ${HOME}/anaconda/bin/conda install -y --name python37_devsim numpy mkl
+
